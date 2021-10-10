43c308f0ee3b03ac64aebfcd4dda52f05f0515dc
LENOVO <deyzaaditya9@gmail.com>
2021-10-10 08:16:42 +0700

    layout

---
+++ app/src/main/java/latihan1/com/deyzaaditya/latihan1.java
@@ -0,0 +1,13 @@
+package latihan1.com.deyzaaditya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class latihan1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan1);
+    }
+}
